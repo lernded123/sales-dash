936b2452a6b0f4a2fe800fe66aab94167b53a82d
Samuel <goodlifesam@gmail.com>
2024-04-12 13:48:51 -0700

    frontend

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require('body-parser')
+const router = require('./routes/router')
 
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4001;
 
 const app = express();
 
 app.use(bodyParser.json())
-app.use(bodyParser)
+app.use(bodyParser.urlencoded({extended:false}))
 
 const corsOptions = {
     origin:'#',
     credentials: true,
     optionSuccessStatus:200
 }
-app.use(cors)
- 
+app.use(cors(corsOptions))
+app.use('/', router)
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+
+function App() {
+ 
+  <h1>hello</h1>
+}
+
+export default App
