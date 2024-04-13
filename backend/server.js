0785e85fac7ae1d93087ee443e44e504bb27e945
Samuel <goodlifesam@gmail.com>
2024-04-13 13:13:19 -0700

    yes

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,22 @@
-// add something
-//soemtheing else
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require('body-parser')
+
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser)
+
+const corsOptions = {
+    origin:'#',
+    credentials: true,
+    optionSuccessStatus:200
+}
+app.use(cors)
+ 
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
